b10afd74662143aaade933b89bcdedefbfad5f20
Yoeri Samwel <yoerisamwel@gmail.com>
2023-10-14 11:17:55 -0500

    adding final adjustments from documents worked on at work

---
+++ .ipynb_checkpoints/README-checkpoint.md
@@ -0,0 +1,2 @@
+# python-api-challenge
+The GWU bootcamp API week 6 challenge. 
--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "4bc707cacbfe928bc0b3715e11e93363"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoaypify_key = "79c38fe270fe4d16942af77a5155b8af"
